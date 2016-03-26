30da08db2a4ccd63b8952fa91c15ba6b2c025609
Alexey Kurganovich <kurganovich.aa@gmail.com>
2016-03-26 18:56:31 +0300

    Second commit. Make DB

---
+++ resources/booking.sql
@@ -0,0 +1,105 @@
+CREATE DATABASE IF NOT EXISTS `booking_db` /*!40100 DEFAULT CHARACTER SET latin1 */;  
+USE `booking_db`;  
+
+DROP TABLE IF EXISTS `Сheck`;
+DROP TABLE IF EXISTS `Reservation`;
+DROP TABLE IF EXISTS `Room`;
+DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `Role`;
+DROP TABLE IF EXISTS `Picture`;
+
+
+CREATE TABLE IF NOT EXISTS `Picture` (
+  `idPicture` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FileName` VARCHAR(45) NOT NULL,
+  `UploadedNname` VARCHAR(45) NOT NULL,
+  `Width` INT NOT NULL,
+  `Height` INT NOT NULL,
+  PRIMARY KEY (`idPicture`),
+  UNIQUE INDEX `idPicture_UNIQUE` (`idPicture` ASC)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Role` (
+	`idRole` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `NameRole` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`idRole`),
+    UNIQUE INDEX `idRole_UNIQUE` (`idRole` ASC),
+    UNIQUE INDEX `NameRole_UNIQUE` (`NameRole` ASC)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+INSERT INTO `role` VALUES(1,'admin'),(2,'user');
+
+CREATE TABLE IF NOT EXISTS `User` (
+  `idUser` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idRole` INT UNSIGNED NOT NULL,
+  `SecondName` VARCHAR(45) NOT NULL,
+  `FirstName` VARCHAR(45) NOT NULL,
+  `Patronymic` VARCHAR(45) NOT NULL,
+  `Username` VARCHAR(45) NOT NULL,
+  `Password` VARCHAR(45) NOT NULL,
+  `DOB` DATE NOT NULL,
+  `Sex` boolean NOT NULL,
+  `PassportSeries` VARCHAR(45) NOT NULL,
+  `PassportN` INT(45) NOT NULL,
+  `IdentificationN` VARCHAR(45) NOT NULL,
+  `Cash` DECIMAL(64,30) NOT NULL,
+  UNIQUE INDEX `idUser_UNIQUE` (`idUser` ASC),
+  UNIQUE INDEX `IdentificationN_UNIQUE` (`IdentificationN` ASC),
+  CONSTRAINT `idRole` FOREIGN KEY (`idRole`) REFERENCES `Role` (`idRole`) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (`idUser`)
+ )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Room` (
+  `idRoom` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `NameRoom` INT NOT NULL,
+  `Roominess` INT NOT NULL,
+  `Price` DECIMAL(64,2) NOT NULL,
+  `NumberOfRooms` INT NOT NULL,
+  `idPicture` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`idRoom`),
+  UNIQUE INDEX `idRoom_UNIQUE` (`idRoom` ASC),
+  CONSTRAINT `idPicture` FOREIGN KEY (`idPicture`) REFERENCES `Picture` (`idPicture`) ON UPDATE CASCADE
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Reservation` (
+  `idReserv` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idUser` INT UNSIGNED NOT NULL,
+  `idRoom` INT UNSIGNED NOT NULL,
+  `Check_inDate` DATE NOT NULL,
+  `Check_outDate` DATE NOT NULL,
+  `Complete` boolean NOT NULL,
+  `InterestPayment` FLOAT NOT NULL,
+  `Arrived` boolean NOT NULL,
+  PRIMARY KEY (`idReserv`),
+  UNIQUE INDEX `idReserv_UNIQUE` (`idReserv` ASC),
+  CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `User` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `idRoom` FOREIGN KEY (`idRoom`) REFERENCES `Room` (`idRoom`) ON DELETE CASCADE ON UPDATE CASCADE
+ )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `Сheck` (
+  `idСheck` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idReserv` INT UNSIGNED NOT NULL,
+  `Payment` DECIMAL(64,2) NOT NULL,
+  PRIMARY KEY (`idСheck`),
+  UNIQUE INDEX `idСheck_UNIQUE` (`idСheck` ASC),
+  UNIQUE INDEX `idReserv_UNIQUE` (`idReserv` ASC),
+  CONSTRAINT `idReserv` FOREIGN KEY (`idReserv`) REFERENCES `Reservation` (`idReserv`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
